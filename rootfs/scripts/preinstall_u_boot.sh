00aa41f6ca9cc74da20ccc2efc00c14980fcc6d8
Rocket13011 <alexandre@jeedom.com>
2022-03-10 14:47:30 +0100

    Update preinstall_u_boot.sh

--- rootfs/scripts/preinstall_u_boot.sh
+++ rootfs/scripts/preinstall_u_boot.sh
@@ -10,5 +10,10 @@ if [[ "$(id -u)" -ne "0" ]]; then
 fi
 
 mkdir -p /tmp/u-boot /usr/lib/u-boot-${SUBBOARD}
+
+if [SUBBOARD = 'rockpi-4b'] then
+    cp -rv /tmp/u-boot/rockpi-4b/* /usr/lib/u-boot-rockpi-4b
+else
 dpkg -X /packages/${SUBBOARD}-*ubootimg*.deb /tmp/u-boot
 cp -rv /tmp/u-boot/usr/lib/u-boot-${SUBBOARD}/* /usr/lib/u-boot-${SUBBOARD}
+fi
