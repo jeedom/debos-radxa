91b8dcbdfbe2ae79a1bf0fa14520f5b34a2cc76c
Rocket13011 <alexandre@jeedom.com>
2022-03-11 09:21:08 +0100

    Update setup_hostname_hosts.sh

--- rootfs/scripts/setup_hostname_hosts.sh
+++ rootfs/scripts/setup_hostname_hosts.sh
@@ -13,32 +13,32 @@ rm -rf /etc/hosts /etc/hostname
 touch /etc/hosts /etc/hostname
 
 
-if [BOARD = 'radxa-zero'] then
-cat <<-EOF > /etc/hostname
-JeedomLuna
-EOF
-cat <<-EOF > /etc/hosts
-127.0.0.1 localhost
-127.0.1.1 jeedomluna
-# The following lines are desirable for IPv6 capable hosts
-#::1     localhost ip6-localhost ip6-loopback
-#fe00::0 ip6-localnet
-#ff00::0 ip6-mcastprefix
-#ff02::1 ip6-allnodes
-#ff02::2 ip6-allrouters
-EOF
+if ((BOARD = 'radxa-zero')); then
+    cat <<-EOF > /etc/hostname
+    JeedomLuna
+    EOF
+    cat <<-EOF > /etc/hosts
+    127.0.0.1 localhost
+    127.0.1.1 jeedomluna
+    # The following lines are desirable for IPv6 capable hosts
+    #::1     localhost ip6-localhost ip6-loopback
+    #fe00::0 ip6-localnet
+    #ff00::0 ip6-mcastprefix
+    #ff02::1 ip6-allnodes
+    #ff02::2 ip6-allrouters
+    EOF
 else
-cat <<-EOF > /etc/hostname
-JeedomAtlas
-EOF
-cat <<-EOF > /etc/hosts
-127.0.0.1 localhost
-127.0.1.1 jeedomatlas
-# The following lines are desirable for IPv6 capable hosts
-#::1     localhost ip6-localhost ip6-loopback
-#fe00::0 ip6-localnet
-#ff00::0 ip6-mcastprefix
-#ff02::1 ip6-allnodes
-#ff02::2 ip6-allrouters
-EOF
+    cat <<-EOF > /etc/hostname
+    JeedomAtlas
+    EOF
+    cat <<-EOF > /etc/hosts
+    127.0.0.1 localhost
+    127.0.1.1 jeedomatlas
+    # The following lines are desirable for IPv6 capable hosts
+    #::1     localhost ip6-localhost ip6-loopback
+    #fe00::0 ip6-localnet
+    #ff00::0 ip6-mcastprefix
+    #ff02::1 ip6-allnodes
+    #ff02::2 ip6-allrouters
+    EOF
 fi
